e365fa501364c3d33c9437e6a6e064a7100affff
iRxnyy99 <teslimkareem97@gmail.com>
2022-10-22 00:55:19 +0100

    Nice

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *print_most_numbers - print 0123456789
+  * owned by KTi
+  *Return: void
+  */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' ||  c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
+}
