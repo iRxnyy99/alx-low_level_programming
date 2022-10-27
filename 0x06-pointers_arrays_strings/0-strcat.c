090ff88fb191435d401457b42189e611bbb55f3b
iRxnyy99 <teslimkareem97@gmail.com>
2022-10-27 23:14:27 +0100

    Nice

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
- * @dest: pointer arg 1
- * @src: pointer arg 2
- * Return: concatenated strings
-*/
+  * _strcat - concatenate two strings
+  * @dest: pointer arg 1
+  * @src: pointer arg 2
+  * Return: concatenated strings
+  */
 
 char *_strcat(char *dest, char *src)
 {
