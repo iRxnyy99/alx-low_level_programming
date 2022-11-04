1dc4b0700b7b9e5818b0eab1b285558893e5a2b8
iRxnyy99 <teslimkareem97@gmail.com>
2022-11-05 00:15:50 +0100

    Cock

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 	int num, digit, sum = 0;
 
 	for (num = 1; num < argc; num++)
+	{
 		for (digit = 0; argv[num][digit]; digit++)
 		{
 			if (argv[num][digit] < '0' || argv[num][digit] > '9')
@@ -24,10 +25,10 @@ int main(int argc, char *argv[])
 			}
 		}
 
-	sum += atoi(argv[num]);
-}
+		sum += atoi(argv[num]);
+	}
 
-printf("%d\n", sum);
+	printf("%d\n", sum);
 
-return (0);
+	return (0);
 }
